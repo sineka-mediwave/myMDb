133a7945f7c802fbdc99dfd9803d1151a774507a
sineka-mediwave <sineka@mindwaveventures.com>
2023-10-10 19:07:07 +0530

    add movieCard from form

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,12 +1,48 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Home from "./components/Home";
 import MovieForm from "./components/MovieForm";
 
+function setToLocalStorage(movies) {
+  localStorage.setItem("My-IMDB", JSON.stringify(movies));
+}
+
+function getFromLocalStorage() {
+  return JSON.parse(localStorage.getItem("My-IMDB")) || [];
+}
+
 function App() {
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([
+    {
+      id: 1,
+      image:
+        "https://m.media-amazon.com/images/M/MV5BODBiMTcwMDktZGUxZi00MjhiLTgwMTQtOTc4MmViZjg5YTVhXkEyXkFqcGdeQXVyMTA4NjE0NjEy._V1_.jpg",
+      title: "Escape Room",
+      year: "2019",
+      rating: "★★★★★",
+    },
+    {
+      id: 2,
+      image:
+        "https://i.pinimg.com/736x/ba/77/bb/ba77bb07526900709dadaecc967aae14.jpg",
+      title: "INSIDIOUS",
+      year: "2017",
+      rating: "★★★★★",
+    },
+  ]);
   const [showAddMovie, setShowAddMovie] = useState(false);
 
+  useEffect(() => setMovies(getFromLocalStorage()), []);
+  useEffect(() => {
+    setToLocalStorage(movies);
+  }, [movies]);
+
+  function handleAddMovie(newMovie) {
+    const updatedMovies = [...movies, newMovie];
+    setMovies(updatedMovies);
+    setToLocalStorage(updatedMovies);
+    setShowAddMovie(false);
+  }
   return (
     <>
       <h1>My IMDB</h1>
@@ -20,8 +56,10 @@ function App() {
       </div>
       <main>
         {showAddMovie ? (
-          <div id="addMovie">
-            <MovieForm />
+          <div className="backgroundForm">
+            <div id="addMovie">
+              <MovieForm onAddMovie={handleAddMovie} />
+            </div>
           </div>
         ) : (
           // <AddMovie onAddMovie={handleAddMovie} />
--- app/src/components/Home.jsx
+++ app/src/components/Home.jsx
@@ -2,28 +2,31 @@ const Home = (props) => {
   const { movies } = props;
   return (
     <>
+      <div class="search-container">
+        <input type="text" id="movie-search" placeholder="Search for a movie" />
+        <button id="search-button">search</button>
+      </div>
       <div className="movie-cards">
         <div className="movie-card">
           <img
-            src="https://m.media-amazon.com/images/M/MV5BODBiMTcwMDktZGUxZi00MjhiLTgwMTQtOTc4MmViZjg5YTVhXkEyXkFqcGdeQXVyMTA4NjE0NjEy._V1_.jpg"
-            alt="Escape Room poster"
+            src="https://resizing.flixster.com/WjuuMiDmu89ljIvxWpGbCilfTqY=/300x300/v2/https://resizing.flixster.com/v7TV6NeSNV0aOWjXGyl5BI056yg=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzL2FjODIxZjUzLTY5OTEtNGRmOS1hMzc4LTg0MjAyOGI1OTFiNS53ZWJw"
+            alt="GRUDGE"
           />
-          <h3>Movie title : Escape Room</h3>
-          <p>release year : 2017</p>
+          <h3>Movie title : GRUDGE</h3>
+          <p>release year : 2019</p>
           <p>Rating: ★★★★★</p>
         </div>
+        {movies.map((movie, index) => (
+          <div className="movie-card" key={index}>
+            <img src={movie.image} alt={movie.title} />
+            <h3>{movie.title}</h3>
+            <p>Year: {movie.year}</p>
+            <p>Rating: {movie.rating}</p>
+          </div>
+        ))}
       </div>
     </>
   );
 };
 
 export default Home;
-
-// {movies.map((movie, index) => (
-//     <div className="movie-card" key={index}>
-//       <img src={movie.image} alt={movie.title} />
-//       <h3>{movie.title}</h3>
-//       <p>Year: {movie.year}</p>
-//       <p>Rating: {movie.rating}</p>
-//     </div>
-//   ))}
