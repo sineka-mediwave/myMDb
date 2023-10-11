f4dcef768f39d43dccd349f0af3acfde0d9904b5
sineka-mediwave <sineka@mindwaveventures.com>
2023-10-11 17:18:03 +0530

    responsive design

--- app/src/components/Home.jsx
+++ app/src/components/Home.jsx
@@ -1,42 +1,72 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+const MovieCard = (props) => {
+  const { movie } = props;
+  return (
+    <>
+      <img src={movie.image} alt={movie.title} />
+      <h3>{movie.title}</h3>
+      <p>Year: {movie.year}</p>
+      <p>
+        Rating:
+        {(() => {
+          const stars = [];
+          for (let i = 0; i < Number(movie.rating); i++) {
+            stars.push(
+              <span key={i} className="star gold">
+                ★
+              </span>
+            );
+          }
+          for (let i = Number(movie.rating); i < 5; i++) {
+            stars.push(
+              <span key={i} className="star">
+                ★
+              </span>
+            );
+          }
+          return stars;
+        })()}
+      </p>
+    </>
+  );
+};
 
 const Home = (props) => {
   const { movies } = props;
   const [searchTerm, setSearchTerm] = useState("");
-  const [filteredMovies, setFilteredMovies] = useState([]);
+  const [filteredMovies, setFilteredMovies] = useState(movies);
+
+  useEffect(() => {
+    const filtered = movies.filter((movie) =>
+      movie.title.toLowerCase().startsWith(searchTerm.toLowerCase())
+    );
+    if (!searchTerm) {
+      setFilteredMovies(movies);
+    }
+    setFilteredMovies(filtered);
+  }, [searchTerm]);
+  const handleSearch = () => {
+    console.log("clicked search");
+  };
   return (
     <>
       <div className="search-container">
-        <input type="text" id="movie-search" placeholder="Search for a movie" />
-        <button id="search-button">search</button>
+        <input
+          type="text"
+          id="movie-search"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search by movie title..."
+        />
+        <button id="search-button" onClick={handleSearch}>
+          search
+        </button>
       </div>
       <div className="movie-cards">
-        {movies.map((movie, index) => (
+        {filteredMovies.map((movie, index) => (
           <div className="movie-card" key={index}>
-            <img src={movie.image} alt={movie.title} />
-            <h3>{movie.title}</h3>
-            <p>Year: {movie.year}</p>
-            <p>
-              Rating:
-              {(() => {
-                const stars = [];
-                for (let i = 0; i < Number(movie.rating); i++) {
-                  stars.push(
-                    <span key={i} className="star gold">
-                      &#9733;
-                    </span>
-                  );
-                }
-                for (let i = Number(movie.rating); i < 5; i++) {
-                  stars.push(
-                    <span key={i} className="star">
-                      &#9733;
-                    </span>
-                  );
-                }
-                return stars;
-              })()}
-            </p>
+            <MovieCard movie={movie} />
           </div>
         ))}
       </div>
