3b38d67fdfef489c3c4368bdd7ff8c7ea3bfa278
sineka-mediwave <sineka@mindwaveventures.com>
2023-10-13 18:37:29 +0530

    added pagination

--- app/src/components/Home.jsx
+++ app/src/components/Home.jsx
@@ -7,6 +7,9 @@ const Home = (props) => {
   const [searchTerm, setSearchTerm] = useState("");
   const [filteredMovies, setFilteredMovies] = useState(movies);
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const moviesPerPage = 4;
+
   useEffect(() => {
     setFilteredMovies(JSON.parse(localStorage.getItem("My-IMDB")) || []);
   }, [movies]);
@@ -21,15 +24,45 @@ const Home = (props) => {
       : setFilteredMovies(filtered);
   }, [searchTerm]);
 
+  const indexOfLastMovie = currentPage * moviesPerPage;
+  const indexOfFirstMovie = indexOfLastMovie - moviesPerPage;
+  const currentMovies = filteredMovies.slice(
+    indexOfFirstMovie,
+    indexOfLastMovie
+  );
+
+  const nextPage = () => {
+    if (indexOfLastMovie < filteredMovies.length) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const prevPage = () => {
+    if (currentPage > 0) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
   return (
     <>
       <SearchBar setSearchTerm={setSearchTerm} />
       <div className="movie-cards">
-        {filteredMovies.map((movie, index) => (
+        {currentMovies.map((movie, index) => (
           <div className="movie-card" key={index}>
             <MovieCard movie={movie} />
           </div>
         ))}
+      </div>
+      <div className="pagination">
+        <button onClick={prevPage} disabled={currentPage <= 1}>
+          Previous Page
+        </button>
+        <button
+          onClick={nextPage}
+          disabled={indexOfLastMovie >= filteredMovies.length}
+        >
+          Next Page
+        </button>
       </div>
     </>
   );
